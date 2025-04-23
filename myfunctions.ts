61d33d9196dbfbb30918bc754116a7c95eabb4da
Brian Dang <long.dang@student.unsw.edu.au>
2025-04-24 03:32:18 +1000

    finished do you really know functions part

---
+++ myfunctions.ts
@@ -0,0 +1,26 @@
+function addTwo(num: number) {
+  //return num + 2;
+  return "hello"
+}
+
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean) {
+
+}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {
+  
+}
+
+let myValue = addTwo(5);
+getUpper("Brian");
+
+
+
+signUpUser("Brian", "Brian@gmail.com", false);
+loginUser("Brian", "Brian@gmail.com")
+
+export {};
