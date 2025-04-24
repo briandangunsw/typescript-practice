1ffd3ff5379ab3d667156f8af0e4db9d70198668
Brian Dang <long.dang@student.unsw.edu.au>
2025-04-25 03:33:55 +1000

    finished the type alias part

--- myObjects.ts
+++ myObjects.ts
@@ -1,20 +1,35 @@
-const User = {
-  name: "Brian",
-  email: "Brian@gmail.com",
-  isActive: true,
+// const User = {
+//   name: "Brian",
+//   email: "Brian@gmail.com",
+//   isActive: true,
+// }
+
+// function createUser({ name: string, isPaid: boolean}) {
+
+// }
+
+// let newUser = { name: "Brian", isPaid: false, email: "Brian@gmail.com" }
+// createUser(newUser);
+
+
+
+// function createCourse():{name: string, price: number}{
+//   return { name: "reactjs", price: 399 }
+// }
+
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
+
 }
 
-function createUser({ name: string, isPaid: boolean}) {
+type Mystring = string 
 
+function createUser(user: User): User {
+  return {name: "", email: "", isActive: true}
 }
 
-let newUser = { name: "Brian", isPaid: false, email: "Brian@gmail.com" }
-createUser(newUser);
-
-
-
-function createCourse():{name: string, price: number}{
-  return { name: "reactjs", price: 399 }
-}
+createUser({name: "", email: "", isActive: true})
 
 export {};
