593ea7ae506cd287527ecbc448b31f035d4df895
Brian Dang <long.dang@student.unsw.edu.au>
2025-04-28 23:41:32 +1000

    finished Private Public part

--- setup/dist/index.js
+++ setup/dist/index.js
@@ -1,10 +1,23 @@
 "use strict";
-class User {
-    constructor(email, name) {
-        this.city = "Sydney";
-        this.email = email;
-        this.name = name;
-    }
-}
-const Brian = new User("Brian@gmail.com", "Brian");
-//Brian.city = "Sydney";
+// // class User {
+// //   public email: string // everything that you don't mark is public.
+// //   private name: string
+// //   readonly city: string = "Sydney"
+// //   constructor(email: string, name: string) {
+// //     this.email = email;
+// //     this.name = name;
+// //   }
+// // }
+// class User {
+//   readonly city: string = "Sydney"
+//   constructor(
+//     public email: string,
+//     public name: string,
+//     // private userId: string
+//   ) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+// const Brian = new User("Brian@gmail.com", "Brian");
+// //Brian.name
--- setup/src/index.ts
+++ setup/src/index.ts
@@ -1,12 +1,24 @@
-class User {
-  email: string
-  name: string
-  readonly city: string = "Sydney"
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
-  }
-}
+// // class User {
+// //   public email: string // everything that you don't mark is public.
+// //   private name: string
+// //   readonly city: string = "Sydney"
+// //   constructor(email: string, name: string) {
+// //     this.email = email;
+// //     this.name = name;
+// //   }
+// // }
 
-const Brian = new User("Brian@gmail.com", "Brian");
-//Brian.city = "Sydney";
+// class User {
+//   readonly city: string = "Sydney"
+//   constructor(
+//     public email: string,
+//     public name: string,
+//     // private userId: string
+//   ) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
+// const Brian = new User("Brian@gmail.com", "Brian");
+// //Brian.name
