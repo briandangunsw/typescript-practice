7ac165900793cf54f129e206627b60fc56c8fe34
Brian Dang <long.dang@student.unsw.edu.au>
2025-04-29 00:01:29 +1000

    finished Getters and Setters part

--- setup/dist/index.js
+++ setup/dist/index.js
@@ -1,23 +1,38 @@
 "use strict";
-// // class User {
-// //   public email: string // everything that you don't mark is public.
-// //   private name: string
-// //   readonly city: string = "Sydney"
-// //   constructor(email: string, name: string) {
-// //     this.email = email;
-// //     this.name = name;
-// //   }
-// // }
 // class User {
+//   public email: string // everything that you don't mark is public.
+//   private name: string
 //   readonly city: string = "Sydney"
-//   constructor(
-//     public email: string,
-//     public name: string,
-//     // private userId: string
-//   ) {
+//   constructor(email: string, name: string) {
 //     this.email = email;
 //     this.name = name;
 //   }
 // }
-// const Brian = new User("Brian@gmail.com", "Brian");
-// //Brian.name
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+        this.city = "Sydney";
+        this.email = email;
+        this.name = name;
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
+const Brian = new User("Brian@gmail.com", "Brian");
+//Brian.name
+// Brian.deleteToken();
--- setup/src/index.ts
+++ setup/src/index.ts
@@ -1,24 +1,48 @@
-// // class User {
-// //   public email: string // everything that you don't mark is public.
-// //   private name: string
-// //   readonly city: string = "Sydney"
-// //   constructor(email: string, name: string) {
-// //     this.email = email;
-// //     this.name = name;
-// //   }
-// // }
-
 // class User {
+//   public email: string // everything that you don't mark is public.
+//   private name: string
 //   readonly city: string = "Sydney"
-//   constructor(
-//     public email: string,
-//     public name: string,
-//     // private userId: string
-//   ) {
+//   constructor(email: string, name: string) {
 //     this.email = email;
 //     this.name = name;
 //   }
 // }
 
-// const Brian = new User("Brian@gmail.com", "Brian");
-// //Brian.name
+class User {
+
+  private _courseCount = 1;
+
+  readonly city: string = "Sydney"
+  constructor(
+    public email: string,
+    public name: string,
+    // private userId: string
+  ) {
+    this.email = email;
+    this.name = name;
+  }
+
+  private deleteToken() {
+    console.log("Token deleted");
+  }
+
+  get getAppleEmail(): string {
+    return `apple${this.email}`
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course count should be more than 1");
+    }
+    this._courseCount = courseNum
+  }
+}
+
+const Brian = new User("Brian@gmail.com", "Brian");
+//Brian.name
+
+// Brian.deleteToken();
