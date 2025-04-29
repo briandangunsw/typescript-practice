c5c7befd0144a359e50f97b6ecdce57f0fb2e2e9
Brian Dang <long.dang@student.unsw.edu.au>
2025-04-30 00:22:07 +1000

    finished generics in array and arrow functions

--- setup/dist/getSearchProducts.js
+++ setup/dist/getSearchProducts.js
@@ -1,9 +1,12 @@
 "use strict";
-// interface Bootle {
-//   brand: string,
-//   type: number
-// }
-// // identityFour<Bootle>({})
-// function getSearchProducts<T>(products: T[]): T {
-//   return 3
-// }
+// identityFour<Bootle>({})
+function getSearchProducts(products) {
+    // do some database operations
+    const myIndex = 3;
+    return products[myIndex];
+}
+const getMoreSearchProducts = (products) => {
+    // do some database operations
+    const myIndex = 4;
+    return products[myIndex];
+};
