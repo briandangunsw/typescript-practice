866e69d8ab1655e7d320bc1ccf043884d986ad8b
Brian Dang <long.dang@student.unsw.edu.au>
2025-04-30 05:16:20 +1000

    finished type narrowing part

---
+++ setup/dist/detection.js
@@ -0,0 +1,30 @@
+"use strict";
+function detectTypes(val) {
+    if (typeof val === "string") {
+        return val.toLowerCase();
+    }
+    return val + 3;
+}
+function provideId(id) {
+    if (!id) {
+        console.log("Please provide ID");
+        return;
+    }
+    id.toLowerCase();
+}
+function printAll(strs) {
+    // !!!!!!!!!!!!!!!!
+    //  DON'T DO THIS!
+    //   KEEP READING
+    // !!!!!!!!!!!!!!!!
+    if (strs) {
+        if (typeof strs === "object") {
+            for (const s of strs) {
+                console.log(s);
+            }
+        }
+        else if (typeof strs === "string") {
+            console.log(strs);
+        }
+    }
+}
